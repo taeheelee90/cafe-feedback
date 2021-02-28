5245a3d47b363ff6946aaae7d35a52df34266acd
taeheelee90 <taehee.lee90@gmail.com>
2021-02-28 12:08:59 +0200

    Activity Finish button

--- app/src/main/java/com/example/feedback/fragment/MainFragment.kt
+++ app/src/main/java/com/example/feedback/fragment/MainFragment.kt
@@ -10,30 +10,14 @@ import androidx.navigation.Navigation
 import com.example.feedback.R
 import kotlinx.android.synthetic.main.fragment_main.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
-class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MainFragment : Fragment(), View.OnClickListener {
 
     lateinit var navController : NavController
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,28 +30,16 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-        btn_forward.setOnClickListener {
-            navController.navigate(R.id.action_mainFragment_to_questionFragment)
-        }
+        btn_forward.setOnClickListener(this)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    // Implement OnClick method
+    override fun onClick(v: View?) {
+        when(v?.id){
+            // Handle btn_forward in main fragment
+            R.id.btn_forward -> {
+                navController.navigate(R.id.action_mainFragment_to_questionFragment)
             }
+        }
     }
 }
